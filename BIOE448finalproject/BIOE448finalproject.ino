c96e699416b0e67b966d62fae493b03a3de899a5
unknown <bisharabrian2@gmail.com>
2025-03-26 15:35:06 -0500

    Added step counting functionality

--- BIOE448finalproject/BIOE448finalproject.ino
+++ BIOE448finalproject/BIOE448finalproject.ino
@@ -1,7 +1,16 @@
 #include <Wire.h> // Necessary for I2C communication
 
 int accel = 0x53; // I2C address for this sensor (from data sheet)
-float x, y, z;
+float x, y, z, acc;
+
+// declare flag for data storage
+bool flag = false;
+
+// define thresholds
+int upper_threshold = 250;
+int lower_threshold = 220;
+
+int steps;
 
 void setup() {
   Serial.begin(9600);
@@ -21,11 +30,32 @@ void loop() {
   y = (Wire.read() | Wire.read() << 8); // Parse y values
   z = (Wire.read() | Wire.read() << 8); // Parse z values
 
-  //Serial.print("x = "); // Print values
-  Serial.println(x);
-  //Serial.print(", y = ");
-  Serial.println(y);
-  //Serial.print(", z = ");
-  Serial.println(z);
+  if (x > 32767) x -= 65536;
+  if (y > 32767) y -= 65536;
+  if (z > 32767) z -= 65536;
+
+ // Serial.print("x = "); // Print values
+ // Serial.print(x);
+ // Serial.print(", y = ");
+ // Serial.print(y);
+ // Serial.print(", z = ");
+ // Serial.println(z);
+
+  acc = sqrt(pow(x,2) + pow(y,2) + pow(z,2)); // Calculate acceleration magnitude
+ // Serial.println(acc);
+
   delay(200);
+
+  if (acc > upper_threshold && flag == false) {
+    steps += 1;
+    flag = true;
+  }
+
+  if (acc < lower_threshold && flag == true) {
+    steps += 1;
+    flag = false;
+  }
+
+  Serial.println(steps);
+
 }
