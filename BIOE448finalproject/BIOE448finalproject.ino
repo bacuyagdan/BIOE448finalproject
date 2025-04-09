569b82347f724976f1d42f7a39b97dc756a2d0f7
Daniel Bacuyag <d.bacuyag7@gmail.com>
2025-04-09 17:32:03 -0500

    Added two-button interface functionality that allows user to select their height and weight, which are then saved as variables for later calculations to determine calories burned.

--- BIOE448finalproject/BIOE448finalproject.ino
+++ BIOE448finalproject/BIOE448finalproject.ino
@@ -6,17 +6,29 @@ BLEService newService("180A"); // Creates the service
 BLEByteCharacteristic readChar("2A58", BLERead); //initialize read characteristic
 BLEByteCharacteristic writeChar("2A57", BLEWrite); //initializae write characteristic
 
+//accelerometer initialize
 int accel = 0x53; // I2C address for this sensor (from data sheet)
 float x, y, z, acc;
-
-// declare flag for data storage
+float kcal;
+// declare steps flag for data storage
 bool flag = false;
-
-// define thresholds
-int upper_threshold = 250;
-int lower_threshold = 220;
-
+// define step counting thresholds
+int steps_upper_threshold = 250;
+int steps_lower_threshold = 220;
 int steps; // initialize steps variable
+
+//buttons initialize
+int SelectButton = A0; // select button pin
+int CycleButton = A4; // cycle button pin
+
+//variables
+String heightOptions[] = {"<60in", "61-65in", "66-72in",">73in"}; //the height range options
+String weightOptions[] = {"<100lbs", "101-130lbs", "131-160lbs", "161-190lbs", "191-220lbs", "221-250lbs", ">251lbs"}; //weight range options
+int heightIndex = 0;
+int weightIndex = 0;
+String height = "";
+String weight = "";
+int inputState = 0; //0=idle, 1=selecting height, 2=selecting weight
 
 void setup() {
   Serial.begin(9600);
@@ -26,6 +38,8 @@ void setup() {
   Wire.write(8); // Get sample measurement
   Wire.endTransmission();
   lcd.begin(16,2); //Initiate LCD in 16x2 configuration
+  pinMode(SelectButton, INPUT_PULLUP);
+  pinMode(CycleButton, INPUT_PULLUP);
 
 //Bluetooth device setup
   BLE.begin();
@@ -38,6 +52,10 @@ void setup() {
   writeChar.writeValue(0);
   BLE.advertise(); // Look for a Bluetooth Connection
   Serial.println("Bluetooth Device Active");
+
+//button setup
+  pinMode(A0,INPUT); //input for the Enter Button
+  pinMode(A4,INPUT); //input for the Cycle Button
 }
 
 void loop() {
@@ -65,27 +83,90 @@ void loop() {
 
   delay(200);
 
-  if (acc > upper_threshold && flag == false) {
+  if (acc > steps_upper_threshold && flag == false) {
     steps += 1;
     flag = true;
   }
 
-  if (acc < lower_threshold && flag == true) {
+  if (acc < steps_lower_threshold && flag == true) {
     steps += 1;
     flag = false;
   }
 
-  Serial.println(steps);
+  //Serial.println(steps);
 
   //prints the current step count on LCD
   lcd.setCursor(0,0);
   lcd.print("Steps:"); 
-  lcd.setCursor(0,1);
+  lcd.setCursor(6,0);
   lcd.print(steps);
 
   //Bluetooth functionality
   BLEDevice central = BLE.central(); //wait for a BLE central
   if (central) {  // if a central is connected to the peripheral
     readChar.writeValue(steps); //Calls the steps value for the central to read from the peripheral
+  }
+
+  //Calorie counting functionality
+  //Button states (active HIGH)
+  bool selectPressed = digitalRead(SelectButton) == HIGH;
+  bool cyclePressed = digitalRead(CycleButton) == HIGH;
+  if (inputState == 0 && selectPressed){
+    //starts height selection
+    lcd.setCursor(0,1);
+    lcd.print("Height:");
+    lcd.setCursor(8,1);
+    lcd.print(heightOptions[heightIndex]); //displays height options
+    inputState = 1; // selecting height state
+    delay(200);
+  }
+  else if (inputState ==1) {
+    if (cyclePressed) {
+      heightIndex = (heightIndex +1) % (sizeof(heightOptions) / sizeof(heightOptions[0])); //goes to next height range option
+      lcd.setCursor(0,1);
+      lcd.print("                ");
+      lcd.setCursor(0,1);
+      lcd.print(heightOptions[heightIndex]); //displays the next height range option
+      delay(200);
+    }
+    if (selectPressed){
+      height = heightOptions[heightIndex]; //saves the currently shown height range as the user's height
+      lcd.setCursor(0,1);
+      lcd.print("                ");
+      lcd.setCursor(0,1);
+      lcd.print("Height saved");
+      Serial.println(height);
+      delay(2000);
+      lcd.setCursor(0,1);
+      lcd.print("                ");
+      lcd.setCursor(0,1);
+      lcd.print("Weight:");
+      lcd.setCursor(8,1);
+      lcd.print(weightOptions[weightIndex]);
+      inputState = 2;
+      delay(200);
+    }
+  }
+  else if (inputState == 2){
+    if (cyclePressed) {
+      weightIndex = (weightIndex +1) % (sizeof(weightOptions) / sizeof(weightOptions[0])); //goes to next weight range option
+      lcd.setCursor(0,1);
+      lcd.print("                ");
+      lcd.setCursor(0,1);
+      lcd.print(weightOptions[weightIndex]); //displays the next weight range option
+      delay(200);
+    }
+    if (selectPressed) {
+      weight = weightOptions[weightIndex]; //saves the currently shown weight range as the user's weight
+      lcd.setCursor(0,1);
+      lcd.print("                ");
+      lcd.setCursor(0,1);
+      lcd.print("Weight saved");
+      Serial.println(weight);
+      delay(2000);
+      lcd.setCursor(0,1);
+      lcd.print("                ");
+      inputState = 0; //resets flag to idle state
+    }
   }
 }
